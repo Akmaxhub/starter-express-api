6f3a8c37ca9ea963a5f177590c71219762651dd3
Ankur Kumar <78653714+Akmaxhub@users.noreply.github.com>
2023-06-07 12:48:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const bodyParser = require('body-parser');
+const client = require('./connection.js')
+const express = require('express');
+const app = express();
+
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.listen(3000, ()=>{
+    console.log("Sever is now listening at port 3000");
 })
-app.listen(process.env.PORT || 3000)
+
+client.connect();
+
+
+// To fetch complete patient list with details
+
+app.get('/medx_voicebot', (req, res)=>{
+    client.query(`Select * from medx_voicebot`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
+
+// To fetch patient details by id.
+
+app.get('/medx_voicebot/:id', (req, res)=>{
+    client.query(`Select * from medx_voicebot where id=${req.params.id}`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
+
+//To add new user
+
+
+app.post('/medx_voicebot', jsonParser, (req, res)=> {
+    const user = req.body;
+    console.log(user);
+    console.log(req);
+    let insertQuery = `insert into medx_voicebot(Medicines, Symptoms, AdverseEvents, NoMedicineReason) 
+                       values('${user.medicines}', '${user.symptoms}', '${user.adverseevents}', '${user.nomedicinereason}')`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
